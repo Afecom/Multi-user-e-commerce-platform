1df6e0655e6eb0bc2bb1c500d74493d0d26c09a1
Nuru Kamil <knuru472@gmail.com>
2025-11-18 17:17:18 +0300

    Order fetching endpoint done

---
+++ src/controllers/order.controller.ts
@@ -0,0 +1,48 @@
+import type { Request, Response } from "express";
+import { z } from 'zod'
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+const get_orders_by_user_id_schema = z.object({
+    user_id: z.string()
+})
+const get_orders_by_id_schema = z.object({
+    id: z.string()
+})
+
+type get_orders_by_user_id_request = z.infer<typeof get_orders_by_user_id_schema>
+type get_orders_by_id_request = z.infer<typeof get_orders_by_id_schema>
+
+export const get_orders_by_user_id = async (req: Request<{}, {}, get_orders_by_user_id_request>, res: Response): Promise<Response> => {
+    const { user_id } = get_orders_by_user_id_schema.parse(req.body)
+    try {
+        const orders = await prisma.orders.findMany({where: {user_id}})
+        if(!orders) return res.status(404).json({message: "Couldn't find orders associated with the user"})
+        return res.status(200).json({
+            message: "Orders fetched successfully",
+            orders
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: "Internal server error",
+            error
+        })
+    }
+}
+export const get_orders_by_id = async (req: Request<{}, {}, get_orders_by_id_request>, res: Response): Promise<Response> => {
+    const { id } = get_orders_by_id_schema.parse(req.body)
+    try {
+        const order = await prisma.orders.findUnique({where: {id}})
+        if(!order) return res.status(404).json({message: "Couldn't find an order with the provided ID"})
+        return res.status(200).json({
+            message: "Order found successfully",
+            order
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: "Internal server error",
+            error
+        })
+    }
+}
